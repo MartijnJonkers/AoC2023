7aa04946415cdf4d66dd0e195444939f67759a08
Martijn <martijn@baulds.nl>
2023-12-01 10:41:04 +0100

    reduced line lenght

--- day1/day1.py
+++ day1/day1.py
@@ -2,7 +2,7 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import aoc
 
-part1 = sum([int(number[0] + number[-1]) for number in ["".join(filter(lambda x: x.isdigit(), map(str, line))) for line in aoc.loadlines()]])
+part1 = sum([int(nr[0] + nr[-1]) for nr in ["".join(filter(lambda x: x.isdigit(), map(str, line))) for line in aoc.loadlines()]])
 
 aoc.result(part1, "unknown")
 
