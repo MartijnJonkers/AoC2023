2486456b5ebce8998af58e704b6e5d07848132f1
Martijn <martijn@baulds.nl>
2023-12-01 10:39:53 +0100

    day1 part 1

--- day1/day1.py
+++ day1/day1.py
@@ -2,9 +2,9 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import aoc
 
-input = aoc.load().read()
-input = aoc.read()
-input = aoc.loadlines()
-input = aoc.loadlines(discard_empty_lines=True)
+part1 = sum([int(number[0] + number[-1]) for number in ["".join(filter(lambda x: x.isdigit(), map(str, line))) for line in aoc.loadlines()]])
 
-aoc.result("unknown", "unknown")
+aoc.result(part1, "unknown")
+
+#oneliner
+#print(sum([int(nr[0] + nr[-1]) for nr in ["".join(filter(lambda x: x.isdigit(), map(str, line))) for line in open("input.txt").readlines()]]))
