c21f35e931a77d158a33f66f379d18dd6d94f092
Martijn <martijn@baulds.nl>
2023-12-10 21:24:54 +0100

    part 2

--- day9/day9.py
+++ day9/day9.py
@@ -7,9 +7,9 @@ file="input.txt"
 
 lines = [[int(val) for val in line.strip().split(' ')] for line in aoc.loadlines(file=file, discard_empty_lines=True)]
 
-
 parts = [0,0]
 
+# part 1
 for line in lines:
   depth = 0
   lasts = []
@@ -21,5 +21,23 @@ for line in lines:
       diffs.append(line[i+1] - line[i])
     line = diffs
   parts[0] += sum(lasts)
+
+# part 2
+for line in lines:
+  depth = 0
+  firsts = []
+  while(len([1 for e in line if e != 0]) != 0):
+    firsts.append(line[0])
+    depth = depth + 1
+    diffs = []
+    for i in range(0, len(line)-1):
+      diffs.append(line[i+1] - line[i])
+    line = diffs
+
+  prev = 0
+  for i in range(len(firsts)-1, -1, -1):
+    prev = firsts[i] - prev
+
+  parts[1] += prev
 
 aoc.result(parts[0], parts[1])
