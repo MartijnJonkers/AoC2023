17090e6667c0151329af247b4d2396d2ac9f9fd5
Martijn <martijn@baulds.nl>
2023-12-15 21:35:09 +0100

    day 12 p1

--- day12/day12.py
+++ day12/day12.py
@@ -2,29 +2,58 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import aoc
 
-file = "example.txt"
-#file = "input.txt"
+#file = "example.txt"
+file = "input.txt"
 lines = aoc.loadlines(file=file, discard_empty_lines=True)
 
+def check(line, groups, matches):
+  if '?' not in line:
+    g = []
+    for j in range(0, len(line)-1):
+      if( line[j] == '.' and line[j+1] == '#' ):
+        g.append(0)
+      elif( line[j] == '#' ):
+        g[-1] = g[-1] + 1
+    if( len(g) != len(groups) ):
+      return matches
+    for j in range(0, len(g)):
+      if( g[j] != groups[j] ):
+        return matches
+    return matches + 1
+  else :
+    for i in range(0, len(line)):
+      if line[i] == '?':
+        for c in ['#', '.']:
+          line[i] = c
+          matches = check(line, groups, matches)
+        line[i] = '?'
+        break
+    return matches
+
 lines = [[num.split(',') for num in line.split(' ')] for line in lines]
-for i in range(0, len(lines)):
-  record = [n for n in lines[i][0][0].split('.') if n != '']
-  lens = [int(n) for n in lines[i][1]]
-  groups = [("#" * int(n)) for n in lines[i][1]]
-  min_group_len = sum([len(g) for g in groups]) + len(groups) - 1
-
-  record = list(".".join(record))
-  record.insert(0, '.')
-  record.append('.')
-  #record = "".join(record)
-  
-  for g in range(len(groups)-1, -1, -1):
-    for offset in range(0, len(record) - min_group_len):
-
-      pass
-
-  pass
 
 
+def run_part():
+  matches = 0
+  c = 0
+  for line in lines:
 
-aoc.result()
+    line[0][0] = "." + line[0][0] + "."
+    matches = check(list(line[0][0]), [int(n) for n in line[1]] , matches)
+    c = c+1
+    print(c,'/',len(lines))
+
+  aoc.result(matches)
+
+# part1
+run_part()
+
+for line in [lines[1]]:
+  l = line[0][0]
+  g = line[1].copy()
+  for i in range(0,4):
+    line[0][0] = line[0][0] + '?' + l 
+    line[1].extend(g)
+
+# part2
+run_part()
