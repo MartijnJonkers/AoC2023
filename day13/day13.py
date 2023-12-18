dbd2fc3bd7565fea292c320b354a9f3a24330b9f
Martijn <martijn@baulds.nl>
2023-12-18 14:32:06 +0100

    minimized more

--- day13/day13.py
+++ day13/day13.py
@@ -2,26 +2,21 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import aoc
 
-part = 2
-blocks = [block.split('\n') for block in aoc.read().strip().split('\n\n')]
-
-def run_block(block):
+def run_block(block, part):
   for factor in [100,1]:                          # first time top down, second time left right
     for y in range(1, len(block)):                  # go over all rows
-      l = min(len(block) - y, y)                      # determin shortest steps to top or bottom
-      above = block[y-l: y][::-1]                       # get rows above a line, reversed
-      below = block[y: y+l]                             # get rows below a line
+      l = min(len(block) - y, y)                      # determine shortest steps to top or bottom
+      above,below = block[y-l: y][::-1], block[y: y+l]  # get rows above(reversed) and below
       if part == 1:                                     # part 1:
         if( above == below):                              # check above and below are the same
           return y * factor                                 # they are the same
       elif part == 2:                                   # part 2:
         ab = zip("".join(above), "".join(below))          # combine
-        diffs = [1 for a,b in ab if a != b]               # get the diffs at index
+        diffs = [1 for a,b in ab if a != b]               # get the diffs
         if( len(diffs) == 1 ):                            # check 1 diff
           return y * factor                                 # exactly 1 diff found, so must be smutge
     block = ["".join(line) for line in list(zip(*block[::-1]))] # rotate the block 90 degrees and run again
 
-result = 0
-for block in blocks:
-  result = result + run_block(block)   # compute result by adding up all results
-aoc.result(result)
+for part in [1,2]:
+  result = sum( run_block(block, part) for block in  [block.split('\n') for block in aoc.read().strip().split('\n\n')] )
+  print("part", part, "=", result)
